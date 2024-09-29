599951cab54f22b55625828f592efaac5383c42c
Mateusz Stawicki <mateusz.stawicki@3lance.pl>
2024-09-29 07:39:55 +0200

    fix azure

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import os
-
 from openai import AzureOpenAI
 from flask import Flask, render_template, request, jsonify, send_file
 from dotenv import load_dotenv
@@ -9,206 +8,164 @@ from pydantic import BaseModel, Field, create_model, ValidationError
 import json
 from typing import Optional
 
-app = Flask(__name__)
+class TaxGPT:
+    def __init__(self):
+        self.app = Flask(__name__)
+        load_dotenv()
+        self.client = AzureOpenAI(azure_endpoint=os.getenv("AZURE_OPENAI_API_BASE"),
+                                  api_key=os.getenv("AZURE_OPENAI_API_KEY"),
+                                  api_version="2023-03-15-preview")
+        self.messages = [{"text": "W czym mogę pomóc?", "sender": "system"}]
+        self.xsd_file = 'schema.xsd'  # Ensure this path is correct and accessible
+        self.output_xml_file = 'output.xml'  # Temporary output XML file path
+        self.field_dict = self.extract_non_nested_fields_to_dict(self.xsd_file)
+        self.DynamicFieldDict = create_model(
+            'DynamicFieldDict',
+            **{key: (Optional[str], Field()) for key in self.field_dict.keys()}
+        )
+        self.populate_sample_data()
+        self.create_routes()
 
-def load_system_prompt(prompt_file):
-    """Read the system prompt from a file."""
-    with open(prompt_file, 'r', encoding='utf-8') as f:
-        return f.read().strip()
+    def load_system_prompt(self, prompt_file):
+        """Read the system prompt from a file."""
+        with open(prompt_file, 'r', encoding='utf-8') as f:
+            return f.read().strip()
 
-def extract_non_nested_fields_to_dict(xsd_file):
-    # Parse the XML schema file
-    tree = ET.parse(xsd_file)
-    root = tree.getroot()
+    def extract_non_nested_fields_to_dict(self, xsd_file):
+        tree = ET.parse(xsd_file)
+        root = tree.getroot()
+        required_fields_dict = {}
+        ns = {'xs': 'http://www.w3.org/2001/XMLSchema'}
 
-    # Initialize an empty dictionary to store required field names with empty string values
-    required_fields_dict = {}
+        for element in root.findall(".//xs:element", ns):
+            field_name = element.get("name")
+            min_occurs = element.get("minOccurs")
+            has_element_children = any(child.tag.endswith("element") for child in element.findall(".//*", ns))
 
-    # Namespace used in XSD files
-    ns = {'xs': 'http://www.w3.org/2001/XMLSchema'}
+            if field_name and (min_occurs is None or min_occurs != "0") and not has_element_children:
+                required_fields_dict[field_name] = ""
 
-    # Extract elements that do not have any <xs:element> children
-    for element in root.findall(".//xs:element", ns):
-        field_name = element.get("name")
-        min_occurs = element.get("minOccurs")
+        return required_fields_dict
 
-        # Check if the element has children that are <xs:element>
-        has_element_children = any(child.tag.endswith("element") for child in element.findall(".//*", ns))
+    def create_xml_from_dict(self, field_dict, output_xml_file):
+        deklaracja = ET.Element("Deklaracja")
+        naglowek = ET.SubElement(deklaracja, "Naglowek")
+        ET.SubElement(naglowek, "KodFormularza", {
+            "kodSystemowy": "PCC-3 (6)",
+            "kodPodatku": "PCC",
+            "rodzajZobowiazania": "Z",
+            "wersjaSchemy": "1-0E"
+        }).text = field_dict.get("KodFormularza", "")
+        ET.SubElement(naglowek, "WariantFormularza").text = field_dict.get("WariantFormularza", "")
+        ET.SubElement(naglowek, "CelZlozenia", {"poz": "P_6"}).text = field_dict.get("CelZlozenia", "")
+        ET.SubElement(naglowek, "Data", {"poz": "P_4"}).text = field_dict.get("Data", "")
+        ET.SubElement(naglowek, "KodUrzedu").text = field_dict.get("KodUrzedu", "")
 
-        # Only add elements that do not have <xs:element> children
-        if field_name and (min_occurs is None or min_occurs != "0") and not has_element_children:
-            required_fields_dict[field_name] = ""
+        podmiot1 = ET.SubElement(deklaracja, "Podmiot1", {"rola": "Podatnik"})
+        osoba_fizyczna = ET.SubElement(podmiot1, "OsobaFizyczna")
+        ET.SubElement(osoba_fizyczna, "NIP").text = field_dict.get("NIP", "")
+        ET.SubElement(osoba_fizyczna, "ImiePierwsze").text = field_dict.get("ImiePierwsze", "")
+        ET.SubElement(osoba_fizyczna, "Nazwisko").text = field_dict.get("Nazwisko", "")
+        ET.SubElement(osoba_fizyczna, "DataUrodzenia").text = field_dict.get("DataUrodzenia", "")
+        ET.SubElement(osoba_fizyczna, "ImieOjca").text = field_dict.get("ImieOjca", "")
+        ET.SubElement(osoba_fizyczna, "ImieMatki").text = field_dict.get("ImieMatki", "")
 
-    return required_fields_dict
+        adres_zamieszkania = ET.SubElement(podmiot1, "AdresZamieszkaniaSiedziby", {"rodzajAdresu": "RAD"})
+        adres_pol = ET.SubElement(adres_zamieszkania, "AdresPol")
+        ET.SubElement(adres_pol, "KodKraju").text = field_dict.get("KodKraju", "PL")
+        ET.SubElement(adres_pol, "Wojewodztwo").text = field_dict.get("Wojewodztwo", "")
+        ET.SubElement(adres_pol, "Powiat").text = field_dict.get("Powiat", "")
+        ET.SubElement(adres_pol, "Gmina").text = field_dict.get("Gmina", "")
+        ET.SubElement(adres_pol, "Ulica").text = field_dict.get("Ulica", "")
+        ET.SubElement(adres_pol, "NrDomu").text = field_dict.get("NrDomu", "")
+        ET.SubElement(adres_pol, "NrLokalu").text = field_dict.get("NrLokalu", "")
+        ET.SubElement(adres_pol, "Miejscowosc").text = field_dict.get("Miejscowosc", "")
+        ET.SubElement(adres_pol, "KodPocztowy").text = field_dict.get("KodPocztowy", "")
 
-def create_xml_from_dict(field_dict, output_xml_file):
-    # Create root  element for the XML file
-    deklaracja = ET.Element("Deklaracja")
+        pozycje_szczegolowe = ET.SubElement(deklaracja, "PozycjeSzczegolowe")
+        for field, value in field_dict.items():
+            if field not in ["NIP", "ImiePierwsze", "Nazwisko", "DataUrodzenia", "ImieOjca", "ImieMatki", "KodKraju", "Wojewodztwo", "Powiat", "Gmina", "Ulica", "NrDomu", "NrLokalu", "Miejscowosc", "KodPocztowy", "Pouczenia"]:
+                ET.SubElement(pozycje_szczegolowe, field).text = value
 
-    # Create Naglowek section
-    naglowek = ET.SubElement(deklaracja, "Naglowek")
-    ET.SubElement(naglowek, "KodFormularza", {
-        "kodSystemowy": "PCC-3 (6)",
-        "kodPodatku": "PCC",
-        "rodzajZobowiazania": "Z",
-        "wersjaSchemy": "1-0E"
-    }).text = field_dict.get("KodFormularza", "")
-    ET.SubElement(naglowek, "WariantFormularza").text = field_dict.get("WariantFormularza", "")
-    ET.SubElement(naglowek, "CelZlozenia", {"poz": "P_6"}).text = field_dict.get("CelZlozenia", "")
-    ET.SubElement(naglowek, "Data", {"poz": "P_4"}).text = field_dict.get("Data", "")
-    ET.SubElement(naglowek, "KodUrzedu").text = field_dict.get("KodUrzedu", "")
+        ET.SubElement(deklaracja, "Pouczenia").text = field_dict.get("Pouczenia", "")
+        rough_string = ET.tostring(deklaracja, 'utf-8')
+        reparsed = minidom.parseString(rough_string)
+        pretty_xml_as_string = reparsed.toprettyxml(indent="  ")
 
-    # Create Podmiot1 section
-    podmiot1 = ET.SubElement(deklaracja, "Podmiot1", {"rola": "Podatnik"})
-    osoba_fizyczna = ET.SubElement(podmiot1, "OsobaFizyczna")
-    ET.SubElement(osoba_fizyczna, "NIP").text = field_dict.get("NIP", "")
-    ET.SubElement(osoba_fizyczna, "ImiePierwsze").text = field_dict.get("ImiePierwsze", "")
-    ET.SubElement(osoba_fizyczna, "Nazwisko").text = field_dict.get("Nazwisko", "")
-    ET.SubElement(osoba_fizyczna, "DataUrodzenia").text = field_dict.get("DataUrodzenia", "")
-    ET.SubElement(osoba_fizyczna, "ImieOjca").text = field_dict.get("ImieOjca", "")
-    ET.SubElement(osoba_fizyczna, "ImieMatki").text = field_dict.get("ImieMatki", "")
+        with open(output_xml_file, 'w', encoding='utf-8') as f:
+            f.write(pretty_xml_as_string)
 
-    # Create AdresZamieszkaniaSiedziby section
-    adres_zamieszkania = ET.SubElement(podmiot1, "AdresZamieszkaniaSiedziby", {"rodzajAdresu": "RAD"})
-    adres_pol = ET.SubElement(adres_zamieszkania, "AdresPol")
-    ET.SubElement(adres_pol, "KodKraju").text = field_dict.get("KodKraju", "PL")
-    ET.SubElement(adres_pol, "Wojewodztwo").text = field_dict.get("Wojewodztwo", "")
-    ET.SubElement(adres_pol, "Powiat").text = field_dict.get("Powiat", "")
-    ET.SubElement(adres_pol, "Gmina").text = field_dict.get("Gmina", "")
-    ET.SubElement(adres_pol, "Ulica").text = field_dict.get("Ulica", "")
-    ET.SubElement(adres_pol, "NrDomu").text = field_dict.get("NrDomu", "")
-    ET.SubElement(adres_pol, "NrLokalu").text = field_dict.get("NrLokalu", "")
-    ET.SubElement(adres_pol, "Miejscowosc").text = field_dict.get("Miejscowosc", "")
-    ET.SubElement(adres_pol, "KodPocztowy").text = field_dict.get("KodPocztowy", "")
+        print("XML file created with populated required fields at:", output_xml_file)
 
-    # Create PozycjeSzczegolowe section
-    pozycje_szczegolowe = ET.SubElement(deklaracja, "PozycjeSzczegolowe")
-    for field, value in field_dict.items():
-        if field not in ["NIP", "ImiePierwsze", "Nazwisko", "DataUrodzenia", "ImieOjca", "ImieMatki", "KodKraju", "Wojewodztwo", "Powiat", "Gmina", "Ulica", "NrDomu", "NrLokalu", "Miejscowosc", "KodPocztowy", "Pouczenia"]:
-            ET.SubElement(pozycje_szczegolowe, field).text = value
+    def populate_sample_data(self):
+        self.field_dict["KodFormularza"] = "PCC-3"
+        self.field_dict["WariantFormularza"] = "6"
+        self.field_dict["CelZlozenia"] = "1"
+        self.field_dict["P_20"] = "1"
+        self.field_dict["Pouczenia"] = "1"
 
-    # Create Pouczenia section
-    ET.SubElement(deklaracja, "Pouczenia").text = field_dict.get("Pouczenia", "")
+    def create_routes(self):
+        @self.app.route('/')
+        def index():
+            return render_template('index.html')
 
-    # Convert the tree to a string and prettify using minidom
-    rough_string = ET.tostring(deklaracja, 'utf-8')
-    reparsed = minidom.parseString(rough_string)
-    pretty_xml_as_string = reparsed.toprettyxml(indent="  ")
+        @self.app.route('/get_messages', methods=['GET'])
+        def get_messages():
+            return jsonify(self.messages)
 
-    # Write the prettified XML to the output file
-    with open(output_xml_file, 'w', encoding='utf-8') as f:
-        f.write(pretty_xml_as_string)
+        @self.app.route('/send_message', methods=['POST'])
+        def send_message():
+            data = request.get_json()
+            user_message = data['text']
+            self.messages.append({"text": user_message, "sender": "user"})
 
-    print("XML file created with populated required fields at:", output_xml_file)
+            system_prompt = self.load_system_prompt('prompt.txt')
+            empty_keys = [key for key, value in self.field_dict.items() if value == ""]
+            extraction_prompt = "oto lista pól, które są wciąż puste: " + ", ".join(empty_keys) + ".\nOraz to co napisał użytkownik: " + f"{self.messages}\n" + "\n"
+            extraction_prompt = extraction_prompt + self.load_system_prompt('prompt1.txt')
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+            try:
+                response = self.client.beta.chat.completions.parse(
+                    model="gpt-4o",
+                    messages=[{"role": "system", "content": extraction_prompt}],
+                    response_format=self.DynamicFieldDict
+                )
 
-@app.route('/get_messages', methods=['GET'])
-def get_messages():
-    return jsonify(messages)
+                parsed_response = response.choices[0].message.parsed
+                response_dict = parsed_response.dict()
+                for key, value in response_dict.items():
+                    if value:
+                        self.field_dict[key] = value
+            except Exception as e:
+                ai_response = "Sorry, there was an error processing your request: " + str(e)
+                self.messages.append({"text": ai_response, "sender": "bot"})
 
-@app.route('/send_message', methods=['POST'])
-def send_message():
-    data = request.get_json()
-    user_message = data['text']
+            empty_keys = [key for key, value in self.field_dict.items() if value == ""]
+            try:
+                response = self.client.chat.completions.create(
+                    model="gpt-4o",
+                    messages=[{"role": "system", "content": f"Oto puste pola: " + ", ".join(empty_keys) + self.load_system_prompt('prompt2.txt') + f"Oto lista poprzednich wiadomości: {self.messages}\n"}],
+                    max_tokens=150
+                )
 
-    # Add user's message to messages
-    messages.append({"text": user_message, "sender": "user"})
+                ai_response = response.choices[0].message.content.strip()
+                self.messages.append({"text": ai_response, "sender": "bot"})
+            except Exception as e:
+                ai_response = "Sorry, there was an error processing your request: " + str(e)
+                self.messages.append({"text": ai_response, "sender": "bot"})
 
-    # Read system prompt from prompt.txt
-    system_prompt = load_system_prompt('prompt.txt')
+            return jsonify(success=True)
 
-    empty_keys = [key for key, value in field_dict.items() if value == ""]
-    print(", ".join(empty_keys))
-    # extraction_prompt = (
-    #     f"Here is a list of fields that are still empty: {empty_keys}.\n"
-    #     f"Based on the following conversation history, fill in these fields where possible:\n"
-    #     f"{messages}\n"
-    #     f"Make sure to never make up information, and only provide accurate data.\n"
-    #     f"Only include the fields that you have values for, and leave the rest out."
-    # )
-    extraction_prompt = "oto lista pól, które są wciąż puste: " + ", ".join(empty_keys) + ".\nOraz to co napisał użytkownik: " + f"{messages}\n" + "\n"
-    extraction_prompt = extraction_prompt + load_system_prompt('prompt1.txt')
+        @self.app.route('/generate_xml', methods=['GET'])
+        def generate_xml():
+            self.create_xml_from_dict(self.field_dict, self.output_xml_file)
+            return send_file(self.output_xml_file, as_attachment=True, download_name='output.xml')
 
-    try:
-        response = client.beta.chat.completions.parse(model="gpt-4o",
-        messages = [
-            {"role": "system", "content": extraction_prompt},
-        ],
-        response_format=DynamicFieldDict)
+    def run(self):
+        self.app.run()
 
-        parsed_response = response.choices[0].message.parsed
-
-        response_dict = parsed_response.dict()
-        print(response_dict)
-        for key, value in response_dict.items():
-            if value:
-                field_dict[key] = value
-                print(f"Updated field_dict[{key}] with value: {value}")
-    except Exception as e:
-        ai_response = "Sorry, there was an error processing your request: " + str(e)
-        print(ai_response)
-        messages.append({"text": ai_response, "sender": "bot"})
-
-    # Get response from Azure OpenAI using ChatCompletion
-    empty_keys = [key for key, value in field_dict.items() if value == ""]
-    print(", ".join(empty_keys))
-    try:
-        response = client.chat.completions.create(model="gpt-4o",
-        messages=[
-            {"role": "system", "content": f"Oto puste pola: " + ", ".join(empty_keys) + load_system_prompt('prompt2.txt') + f"Oto lista poprzednich wiadomości: {messages}\n"},
-            # {"role": "user", "content": user_message}
-        ],
-        max_tokens=150)
-
-        # Extract the assistant's response
-        ai_response = response.choices[0].message.content.strip()
-        messages.append({"text": ai_response, "sender": "bot"})
-    except Exception as e:
-        ai_response = "Sorry, there was an error processing your request: " + str(e)
-        messages.append({"text": ai_response, "sender": "bot"})
-
-    return jsonify(success=True)
-
-@app.route('/generate_xml', methods=['GET'])
-def generate_xml():
-    # Create the XML file using the dictionary values
-    create_xml_from_dict(field_dict, output_xml_file)
-
-    # Send the generated XML file as a download
-    return send_file(output_xml_file, as_attachment=True, download_name='output.xml')
+tax_app = TaxGPT()
+app = tax_app.app
 
 if __name__ == "__main__":
-    client = AzureOpenAI(azure_endpoint=os.getenv("AZURE_OPENAI_API_BASE"),
-    api_key=os.getenv("AZURE_OPENAI_API_KEY"),
-    api_version="2023-03-15-preview")
-
-    # Load environment variables from .env file
-    load_dotenv()
-
-    # In-memory storage for chat messages
-    messages = [{"text": "W czym mogę pomóc?", "sender": "system"}]
-
-    # Extract fields into a dictionary with empty values
-    xsd_file = 'schema.xsd'  # Ensure this path is correct and accessible
-    output_xml_file = 'output.xml'  # Temporary output XML file path
-
-    # Create the dictionary from the XSD file
-    field_dict = extract_non_nested_fields_to_dict(xsd_file)
-
-    # Create a dynamic Pydantic model based on field_dict
-    DynamicFieldDict = create_model(
-        'DynamicFieldDict',
-        **{key: (Optional[str], Field()) for key in field_dict.keys()}
-    )
-
-    # Populate the dictionary with sample values (customize as needed)
-    field_dict["KodFormularza"] = "PCC-3"
-    field_dict["WariantFormularza"] = "6"
-    field_dict["CelZlozenia"] = "1"
-    field_dict["P_20"] = "1"
-    field_dict["Pouczenia"] = "1"
-
-    app.run()
+    tax_app.run()
